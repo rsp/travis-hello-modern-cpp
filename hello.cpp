bc72ffa93f15b59d3a95d54f859d6e4aca5acb7e
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-30 12:24:30 +0100

    hello.cpp

---
+++ hello.cpp
@@ -0,0 +1,11 @@
+// Travis example for C++ by Rafał Pocztarski
+// For more languages and info see:
+// https://github.com/rsp/travis-hello#readme
+
+#include <iostream>
+
+int main() {
+
+    std::cout << "Hello" << std::endl;
+
+}
