50521a06a4c1ebe6755a1256ca9e5c1e966a3a8f
Rafał Pocztarski <r.pocztarski@gmail.com>
2016-10-30 12:51:28 +0100

    update test

--- test.sh
+++ test.sh
@@ -1,7 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
 # Travis example for C++ by Rafał Pocztarski
 # For more languages and info see:
 # https://github.com/rsp/travis-hello#readme
 
-echo test stub
+pass ( ) {
+    echo Test $t OK
+    t=$(($t+1))
+}
+fail ( ) {
+    echo Test $t FAILED
+    exit 1
+}
+
+cmp <(./z01) <(echo $z01o) && pass || fail
+
+echo ALL TESTS PASSED
